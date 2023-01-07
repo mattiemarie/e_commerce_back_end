150a1941ad3fdc4014b9d0e2363f89c6a0354dcb
Mattie Gonzalez <gonzalem@lssc.edu>
2023-01-06 21:11:49 -0500

    index.js associations setup

--- models/index.js
+++ models/index.js
@@ -4,13 +4,44 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//Check To See if these are setup Correctly
+
 // Products belongsTo Category
+Category.hasOne(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.hasOne(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsTo(ProductTag, {
+  foreignKey: '',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.hasMany(ProductTag, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsTo(ProductTag, {
+  foreignKey: '',
+});
+
 
 module.exports = {
   Product,
