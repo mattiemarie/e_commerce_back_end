0d775a028c1345314138dcacefe5945f9c55cbda
Mattie Gonzalez <gonzalem@lssc.edu>
2023-01-06 20:55:23 -0500

    add Tag Model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,37 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: '10',
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
