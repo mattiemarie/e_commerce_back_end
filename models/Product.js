23d880542aef505d3f7b54d12ea584dfa0ab9755
Mattie Gonzalez <gonzalem@lssc.edu>
2023-01-06 20:52:49 -0500

    add Product Model

--- models/Product.js
+++ models/Product.js
@@ -22,7 +22,24 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      isDecimal: true
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: '10',
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
